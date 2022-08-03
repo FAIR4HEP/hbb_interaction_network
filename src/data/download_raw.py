0a852909a27cf93303d6b75fa69096154c00174d
Javier Duarte <jduarte@ucsd.edu>
2022-08-02 20:26:27 -0700

    black/flake8

--- src/data/download_raw.py
+++ src/data/download_raw.py
@@ -1,6 +1,7 @@
 import sys
 import urllib.request
 from pathlib import Path
+
 from tqdm import tqdm
 
 # prepare the dir for raw data
@@ -9,30 +10,26 @@ data_raw_dir = cwd.parent.parent.joinpath("data/raw")
 data_raw_dir.mkdir(parents=True, exist_ok=True)
 
 # list the online raw files
-rawFileUrls = ["http://opendata.cern.ch/eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_0.h5",
-                "http://opendata.cern.ch/eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_1.h5",
-                "http://opendata.cern.ch/eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_2.h5",
-                "http://opendata.cern.ch/eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_3.h5",
-                "http://opendata.cern.ch/eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_4.h5",
-                "http://opendata.cern.ch/eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_5.h5",
-                "http://opendata.cern.ch/eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_6.h5",
-                "http://opendata.cern.ch/eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_7.h5",
-                "http://opendata.cern.ch/eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/test/ntuple_merged_8.h5"]
+base_url = "http://opendata.cern.ch/eos/opendata/cms/datascience/HiggsToBBNtupleProducerTool/test"
+rawFileUrls = ["{base_url}/ntuple_merged_{i}".format(base_url=base_url, i=i) for i in range(0, 9)]
 
 # prepare target file objects
 fileNames = [url.rsplit("/", 1)[-1] for url in rawFileUrls]
 targetFiles = [data_raw_dir.joinpath(f) for f in fileNames]
 
+
 # progress bar class and fn for each file
 class DownloadProgressBar(tqdm):
     def update_to(self, b=1, bsize=1, tsize=None):
         if tsize is not None:
-            self.total=tsize
-        self.update(b*bsize-self.n)
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
 
 def download_url(url, output_path):
-    with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=url.split('/')[-1]) as t:
+    with DownloadProgressBar(unit="B", unit_scale=True, miniters=1, desc=url.split("/")[-1]) as t:
         urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+
 
 testLimit = 2
 counter = 0
@@ -44,6 +41,3 @@ for url, f in zip(rawFileUrls, targetFiles):
         continue
     else:
         sys.exit(1)
-
-
-

f7587de38bfc16cd468306f4c6dce5691272e019
Javier Duarte <jduarte@ucsd.edu>
2022-08-15 13:04:54 -0700

    update

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -17,7 +17,8 @@ def to_np_array(ak_array, maxN=100, pad=0, dtype=float):
 
 @click.command()
 @click.argument("definitions", type=click.Path(exists=True), default="definitions.yml")
-def main(definitions):
+@click.option("--train", is_flag=True, show_default=True, default=True)
+def main(definitions, train):
     """Runs data processing scripts to turn raw data from (../raw) into
     cleaned data ready to be analyzed (saved in ../processed).
     """
@@ -36,18 +37,27 @@ def main(definitions):
     labels = defn["labels"]
     n_feature_sets = defn["n_feature_sets"]
     batch_size = defn["batch_size"]
+    if train:
+        dataset = "train"
+    else:
+        dataset = "test"
+    files = defn[f"{dataset}_files"]
 
     counter = 0
-    for input_file in defn["train_files"]:
+    for input_file in files:
         in_file = uproot.open(input_file)
         tree = in_file[defn["tree_name"]]
         nentries = tree.num_entries
         logger.info(f"opening {input_file} with {nentries} events")
         for counter, k in enumerate(range(0, nentries, batch_size)):
+            arrays = tree.arrays(spectators, library="np", entry_start=k, entry_stop=k + batch_size)
+            spec_array = np.expand_dims(np.stack([arrays[spec] for spec in spectators], axis=1), axis=1)
+            real_batch_size = spec_array.shape[0]
+
             feature_arrays = {}
             for j in range(n_feature_sets):
                 feature_arrays[f"features_{j}"] = np.zeros(
-                    (batch_size, defn[f"nobj_{j}"], len(defn[f"features_{j}"])),
+                    (real_batch_size, defn[f"nobj_{j}"], len(defn[f"features_{j}"])),
                     dtype=float,
                 )
                 arrays = tree.arrays(
@@ -62,11 +72,8 @@ def main(definitions):
                 # For PyTorch channels-first style networks
                 np.swapaxes(feature_arrays[f"features_{j}"], 1, 2)
 
-            arrays = tree.arrays(spectators, library="np", entry_start=k, entry_stop=k + batch_size)
-            spec_array = np.expand_dims(np.stack([arrays[spec] for spec in spectators], axis=1), axis=1)
-
             arrays = tree.arrays(labels, library="np", entry_start=k, entry_stop=k + batch_size)
-            target_array = np.zeros((batch_size, 2))
+            target_array = np.zeros((real_batch_size, 2), dtype=float)
             target_array[:, 0] = arrays["sample_isQCD"] * (
                 arrays["label_QCD_b"]
                 + arrays["label_QCD_bb"]
@@ -76,15 +83,15 @@ def main(definitions):
             )
             target_array[:, 1] = arrays["label_H_bb"]
 
-            with h5py.File(f"{project_dir}/data/processed/newdata_{counter}.h5", "w") as h5:
-                logger.info(f"creating {h5.filename} h5 file with {batch_size} events")
-                training_data = h5.create_group("training_subgroup")
+            with h5py.File(f"{project_dir}/data/processed/{dataset}/newdata_{counter}.h5", "w") as h5:
+                logger.info(f"creating {h5.filename} h5 file with {real_batch_size} events")
+                feature_data = h5.create_group(f"{dataset}ing_subgroup")
                 target_data = h5.create_group("target_subgroup")
                 # weight_data = h5.create_group("weight_subgroup")
                 spec_data = h5.create_group("spectator_subgroup")
                 for j in range(n_feature_sets):
-                    training_data.create_dataset(
-                        f"training_{j}",
+                    feature_data.create_dataset(
+                        f"{dataset}ing_{j}",
                         data=feature_arrays[f"features_{j}"],
                     )
                 target_data.create_dataset("target", data=target_array)

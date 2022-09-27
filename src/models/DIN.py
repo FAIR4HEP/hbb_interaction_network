c86f54a20c9cb2b68d5acbcd0dc9d9b0639560c6
Billy Li <billyli@pop-os.localdomain>
2022-09-26 20:05:35 -0700

    update

--- src/models/DIN.py
+++ src/models/DIN.py
@@ -3,4 +3,21 @@ import itertools
 import torch
 import torch.nn as nn
 
-class IN(nn.Module)
+class interaction(nn.Module):
+    def __init__(
+        self,
+        n_p,
+        n_v,
+        params_p,
+        params_v,
+        hidden,
+        n_pout,
+        n_vout,
+        device="cpu",
+    }:
+        super(interaction, self).__init__()
+        self.hidden = int(hidden)
+        self.P = params_p
+
+
+

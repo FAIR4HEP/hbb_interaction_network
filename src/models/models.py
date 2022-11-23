d14b751fde651d9feb5c6ccf8bd29fa8015107aa
Javier Duarte <jduarte@ucsd.edu>
2022-11-22 19:52:41 -0800

    try finetuning

--- src/models/models.py
+++ src/models/models.py
@@ -186,6 +186,92 @@ class GraphNet(nn.Module):
         return torch.mm(x.reshape(-1, x_shape[2]), y).reshape(-1, x_shape[1], y_shape[1])
 
 
+class GraphNetSingle(nn.Module):
+    def __init__(
+        self,
+        n_constituents,
+        n_targets,
+        params,
+        hidden,
+        De=5,
+        Do=6,
+        softmax=False,
+        device="cpu",
+    ):
+        super(GraphNet, self).__init__()
+        self.hidden = int(hidden)
+        self.P = params
+        self.N = n_constituents
+        self.Nr = self.N * (self.N - 1)
+        self.De = De
+        self.Do = Do
+        self.device = device
+        self.n_targets = n_targets
+        self.assign_matrices()
+        self.softmax = softmax
+
+        self.fr1 = nn.Linear(2 * self.P + self.Dr, self.hidden).to(self.device)
+        self.fr2 = nn.Linear(self.hidden, int(self.hidden)).to(self.device)
+        self.fr3 = nn.Linear(int(self.hidden), self.De).to(self.device)
+        self.fo1 = nn.Linear(self.P + self.Dx + (2 * self.De), self.hidden).to(self.device)
+        self.fo2 = nn.Linear(self.hidden, int(self.hidden)).to(self.device)
+        self.fo3 = nn.Linear(int(self.hidden), self.Do).to(self.device)
+
+        self.fc_fixed = nn.Linear(self.Do, self.n_targets).to(self.device)
+
+    def assign_matrices(self):
+        self.Rr = torch.zeros(self.N, self.Nr)
+        self.Rs = torch.zeros(self.N, self.Nr)
+        receiver_sender_list = [i for i in itertools.product(range(self.N), range(self.N)) if i[0] != i[1]]
+        for i, (r, s) in enumerate(receiver_sender_list):
+            self.Rr[r, i] = 1
+            self.Rs[s, i] = 1
+        self.Rr = (self.Rr).to(self.device)
+        self.Rs = (self.Rs).to(self.device)
+
+    def forward(self, x):
+        # PF Candidate - PF Candidate
+        Orr = self.tmul(x, self.Rr)
+        Ors = self.tmul(x, self.Rs)
+        B = torch.cat([Orr, Ors], 1)
+        # First MLP
+        B = torch.transpose(B, 1, 2).contiguous()
+        B = nn.functional.relu(self.fr1(B.view(-1, 2 * self.P + self.Dr)))
+        B = nn.functional.relu(self.fr2(B))
+        E = nn.functional.relu(self.fr3(B).view(-1, self.Nr, self.De))
+        del B
+        E = torch.transpose(E, 1, 2).contiguous()
+        Ebar_pp = self.tmul(E, torch.transpose(self.Rr, 0, 1).contiguous())
+        del E
+
+        # Final output matrix for particles
+        C = torch.cat([x, Ebar_pp], 1)
+        del Ebar_pp
+        C = torch.transpose(C, 1, 2).contiguous()
+        # Second MLP
+        C = nn.functional.relu(self.fo1(C.view(-1, self.P + self.Dx + (2 * self.De))))
+        C = nn.functional.relu(self.fo2(C))
+        Omatrix = nn.functional.relu(self.fo3(C).view(-1, self.N, self.Do))
+        del C
+
+        # Taking the sum of over each particle/vertex
+        N = torch.sum(Omatrix, dim=1)
+        del Omatrix
+
+        # Classification MLP
+        N = self.fc_fixed(N)
+
+        if self.softmax:
+            N = nn.Softmax(dim=-1)(N)
+
+        return N
+
+    def tmul(self, x, y):  # Takes (I * J * K)(K * L) -> I * J * L
+        x_shape = x.size()
+        y_shape = y.size()
+        return torch.mm(x.reshape(-1, x_shape[2]), y).reshape(-1, x_shape[1], y_shape[1])
+
+
 class GraphNetEmbedding(nn.Module):
     def __init__(
         self,
